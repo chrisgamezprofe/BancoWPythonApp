668c29fa0a369d678debe481154ad32ae33e6cdb
Christian Gámez <cgamez@atlasdev.uy>
2023-09-04 19:10:18 -0500

    first commit

---
+++ data/historial.py
@@ -0,0 +1,19 @@
+import conexion as con
+
+class HistorialData():
+
+    def buscarPorFecha(self,fechaDesde,fechaHasta,tipo,documento):
+        self.db = con.Conexion().conectar()
+        self.cursor = self.db.cursor()
+        sql="""
+        SELECT T.id as transaccion,D.*,T.verificado FROM transferencias T 
+        INNER JOIN depositos D ON D.tipo=T.tipo and D.documento=T.documento
+        WHERE T.fecha_registro>='{}' and T.fecha_registro<='{}' and D.documento='{}' and D.tipo='{}' 
+        and T.motivo=D.motivo and T.monto=D.monto
+        """.format(fechaDesde,fechaHasta,documento,tipo)
+        print(sql)
+        res = self.cursor.execute(sql)
+        data = res.fetchall()
+        return data
+            
+
